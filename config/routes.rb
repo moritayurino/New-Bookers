1ef3dbe81973c8c43577efb3564016b476a26ddc
EC2 Default User <ec2-user@ip-172-31-30-220.ap-northeast-1.compute.internal>
2024-10-02 11:57:03 +0000

    [Fix]4-16

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'homes/top'
-  get 'top' => 'homes#top'
-  post 'top' => 'homes#top'
-  get 'books/new'
-  post 'top' => 'books#top'
+  get '/' => 'homes#top'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get '/books' => 'books#index'
+  get '/show' => 'books#show'
+  get '/edit' => 'books#edit'
+  get '/:id'=> 'books#show', as: 'book'
+  get '/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
