5efe5f00bd1db5c957d95bcefbf5739e4e2b6908
EC2 Default User <ec2-user@ip-172-31-30-220.ap-northeast-1.compute.internal>
2024-10-03 11:55:27 +0000

    [Update]4-16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,40 +1,51 @@
 class BooksController < ApplicationController
-  
+
   def top
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice] = "Can't be blank"
+      render :show
+    end
   end
 
   def show
     @book = Book.find(params[:id])
-    redirect_to book_path(book.id)
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
-  end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice] = "Can't be blank"
+      render :edit
+    end
+  end
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
   def book_params
   params.require(:book).permit(:title, :body)
